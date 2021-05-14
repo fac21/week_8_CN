9baaebd42607fc9faa04f29521b5f40c34310334
chunzg <chunzhang53@gmail.com>
2021-05-14 12:23:58 +0100

    Combine two components to make both movies refresh at same time and enable score
    Co-authored-by: ByteSized <60395899+ByteSizedIT@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import popcorn from './popcorn.png'
 import pixel from './pixel.png'
 import './App.css'
-import {RandomMovie, FetchChosenMovie} from './Ghibli.jsx'
+import FetchMovie from './Ghibli.jsx'
 
 function App() {
   const [chosenMovie, setChosenMovie] = useState("")
@@ -18,30 +17,37 @@ function App() {
               }
             }>
           <label htmlFor="cars"><p>Choose a movie:</p>
-            <select 
-            name="movies" 
-            id="movies" 
-            >
+            <select name="movies" id="movies">
+              <option value="Arrietty">Arrietty</option>
+              <option value="Castle in the Sky">Castle in the Sky</option>
+              <option value="From Up on Poppy Hill">From Up on Poppy Hill</option>
+              <option value="Grave of the Fireflies">Grave of the Fireflies</option>
+              <option value="Howl's Moving Castle">Howl's Moving Castle</option>
               <option value="Kiki's Delivery Service">Kiki's Delivery Service</option>
+              <option value="My Neighbors the Yamadas">My Neighbors the Yamadas</option>
               <option value="My Neighbor Totoro">My Neighbor Totoro</option>
               <option value="Only Yesterday">Only Yesterday</option>
-              <option value="Grave of the Fireflies">Grave of the Fireflies</option>
-              <option value="Porco Rosso">Porco Rosso</option>
               <option value="Pom Poko">Pom Poko</option>
-              <option value="Whisper of the Heart">Whisper of the Heart</option>
+              <option value="Porco Rosso">Porco Rosso</option>
+              <option value="Ponyo">Ponyo</option>
+              <option value="Princess Mononoke">Princess Mononoke</option>
+              <option value="Spirited Away">Spirited Away</option>
+              <option value="Tales from Earthsea">Tales from Earthsea</option>
+              <option value="The Cat Returns">The Cat Returns</option>
+              <option value="The Red Turtle">The Red Turtle</option>
+              <option value="The Tale of the Princess Kaguya">The Tale of the Princess Kaguya</option>
               <option value="The Wind Rises">The Wind Rises</option>
+              <option value="When Marnie Was There">When Marnie Was There</option>
+              <option value="Whisper of the Heart">Whisper of the Heart</option>
             </select>
           </label>
           <button type='submit'>Go!</button>
         </form>
         {chosenMovie ? 
-          <div class='flex-movies'>
-            <FetchChosenMovie chosenMovie={chosenMovie}/>
-            <RandomMovie /> 
+          <div className='flex-movies'>
+            <FetchMovie chosenMovie={chosenMovie}/>
           </div>
           : null}
-        <br></br>
-        <p>Popcorn Points</p><img src={popcorn} height='65'/>
       </header>
     </div>
   )
--- src/Ghibli.jsx
+++ src/Ghibli.jsx
@@ -1,64 +1,73 @@
 import React from 'react';
+import popcorn from './popcorn.png'
 
-function FetchChosenMovie(props) {
-  const [movieData, setMovieData] = React.useState('');
+function FetchMovie(props) {
+  const [movieA, setMovieA] = React.useState(); //movies are objects
+  const [movieB, setMovieB] = React.useState();
+  const [score, setScore] = React.useState(0);
+  const [lastScore, setLastScore] = React.useState(0);
 
   React.useEffect(() => {
     fetch("https://ghibliapi.herokuapp.com/films")
       .then((movieList) => movieList.json())
       .then((movieList) => {
-        setMovieData(movieList);
-        const filteredMovie = movieList.filter((item) => {
+
+        //Find the to be movieA
+        const filteredMovieArr = movieList.filter((item) => {
           if(item.title === props.chosenMovie) {
             return props.chosenMovie;
           }
         })
-        setMovieData(filteredMovie[0]);
-      });
-  }, [props.chosenMovie]);
+        const chosenMovie = (filteredMovieArr[0]) //this variable is updated and available straight away
+        setMovieA(chosenMovie); // SetMovieA doesnt take effect until we have returned out of the component. Therefore is we conole logged it, it would show the last movie details, or undefined it the first time user plays.
 
-  if (!movieData) return <div>Loading...</div>;
-  return (
-    <div>
-      <h2>{movieData.title}</h2>
-      <h3>{movieData.original_title}</h3>
-      <h4>Rotten Tomato Rating:</h4> 
-      <h3 className='rt'>{movieData.rt_score}</h3>
-      <h4>Released: {movieData.release_date}</h4>
-      <h4>Running Time: {movieData.running_time}</h4>
-  </div>
-  );
-}
-
-
-function RandomMovie() {
-  const [movieData, setMovieData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("https://ghibliapi.herokuapp.com/films")
-      .then((movieList) => movieList.json())
-      .then((movieList) => {
-        console.log(movieList)
-        setMovieData(movieList)
-        //Select a random movie from the array
+        //Find the const movieB 
         const randItemNo = Math.floor(Math.random()*movieList.length);
-        const movieData = movieList[randItemNo];
-        setMovieData(movieData);
-      });
-  }, []);
+        const randomMovie = movieList[randItemNo];
+        setMovieB(randomMovie);
 
-  if (!movieData) return <div>Loading...</div>;
+        if(parseInt(chosenMovie.rt_score) > parseInt(randomMovie.rt_score)) {
+          console.log('win')
+          setScore(score+1)
+        }
+        else {
+          console.log('defeat')
+          setLastScore(score);
+          setScore(0);
+
+        }
+      })
+  }, [props.chosenMovie]); //gets called everytime chosenMovie changes
+
+  //Need check whether this works beyond first time a new film is selected and rendered
+  if (!movieA || !movieB) return <div>Loading...</div>;
+  //If line above does not run, then statement below will return instead
   return (
     <div>
-      <h2>{movieData.title}</h2>
-      <h3>{movieData.original_title}</h3>
-      <h4>Rotten Tomato Rating:</h4> 
-      <h3 className='rt'>{movieData.rt_score}</h3>
-      <h4>Released: {movieData.release_date}</h4>
-      <h4>Running Time: {movieData.running_time}</h4>
+      <div className='flex-movies'>
+        <div>
+        <h2>{movieA.title}</h2>
+        <h3>{movieA.original_title}</h3>
+        <h4>Rotten Tomato Rating:</h4> 
+        <h3 className='rt'>{movieA.rt_score}</h3>
+        <h4>Released: {movieA.release_date}</h4>
+        <h4>Running Time: {movieA.running_time}</h4>
+        </div>
+        <div>
+          <h2>{movieB.title}</h2>
+          <h3>{movieB.original_title}</h3>
+          <h4>Rotten Tomato Rating:</h4> 
+          <h3 className='rt'>{movieB.rt_score}</h3>
+          <h4>Released: {movieB.release_date}</h4>
+          <h4>Running Time: {movieB.running_time}</h4>
+        </div>
+        <br></br>
+      </div>
+      <img src={popcorn} height='65'/> 
+      <p>Consecutive Popcorn Points</p>
+      {score === 0? <p className='rt'>Game Over!! You scored {lastScore} point(s)</p> : <p className='rt'>{score}</p>}
     </div>
   );
 }
 
-
-export { FetchChosenMovie, RandomMovie }
+export default FetchMovie
